e347d666a5850edaeaae6e2f0db09e2d5429cc3f
rd-dhyoo <71996956+rd-dhyoo@users.noreply.github.com>
2022-08-03 13:57:13 +0900

    Add Engine
    version

---
+++ src/MarkNet.Core/Engines/Engine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkNet.Core.Engines
+{
+    public abstract class Engine
+    {
+        public virtual Task InitalizeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task RunAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                await InitalizeAsync();
+
+                await StartAsync();
+
+                while (stoppingToken.IsCancellationRequested == false)
+                {
+                    await RunOneCycleAsync();
+                }
+            }
+            catch (AggregateException ae)
+            {
+                foreach (Exception e in ae.InnerExceptions)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                await StopAsync();
+            }
+        }
+
+        public virtual Task StartAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public abstract Task RunOneCycleAsync();
+
+
+        public virtual Task StopAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
