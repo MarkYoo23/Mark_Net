0b791bad0abaf229f9f435f6143839a25eef9352
rd-dhyoo <71996956+rd-dhyoo@users.noreply.github.com>
2022-08-05 11:38:53 +0900

    Add Property Model Clear Feature

---
+++ test/BaseNet.Core.Test/Models/MockPropertyModel.cs
@@ -0,0 +1,14 @@
+﻿using MarkNet.Core.Models;
+
+namespace BaseNet.Core.Test.Models
+{
+    public class MockPropertyModel : PropertyModel<MockPropertyModel>
+    {
+        public int NormalInt { get; set; }
+        public int? NullableInt { get; set; }
+        public string NormalString { get; set; } = null!;
+        public string? NullableString { get; set; }
+        public DateTime NormalTime { get; set; }
+        public DateTime? NullableTime { get; set; }
+    }
+}
