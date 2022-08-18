2f2123d849d33b65099525b36fc14c30507d1758
rd-dhyoo <71996956+rd-dhyoo@users.noreply.github.com>
2022-08-18 14:29:47 +0900

    Fix Collection Config Save Bug

--- test/MarkNet.Infrastructure.Test/UnitTests/Services/Configs/CollectionConfigTests.cs
+++ test/MarkNet.Infrastructure.Test/UnitTests/Services/Configs/CollectionConfigTests.cs
@@ -1,4 +1,5 @@
-﻿using MarkNet.Test.Services;
+﻿using MarkNet.Test.Entities;
+using MarkNet.Test.Services;
 using MarkNet.Test.Services.Configs;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +25,107 @@ namespace MarkNet.Test.UnitTests.Configs
             {
                 var service = serviceProvider.GetRequiredService<FakeCollectionConfigService>();
                 await service.InitializeAsync();
+            });
+
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public async Task FakeCollectionConfigService_SetShould()
+        {
+            using var scope = _host.Services.CreateScope();
+            var serviceProvider = scope.ServiceProvider;
+
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                var service = serviceProvider.GetRequiredService<FakeCollectionConfigService>();
+
+                var configs = new FakeCollectionConfigEntity[]
+                {
+                    new FakeCollectionConfigEntity()
+                    {
+                        Id = 0,
+                        Number = 1,
+                        Value = 1,
+                    },
+                    new FakeCollectionConfigEntity()
+                    {
+                        Id = 0,
+                        Number = 2,
+                        Value = 2,
+                    },
+                    new FakeCollectionConfigEntity()
+                    {
+                        Id = 0,
+                        Number = 3,
+                        Value = 3,
+                    },
+                };
+
+                await service.SetAsync(configs);
+            });
+
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public async Task FakeCollectionConfigService_SetTwiceShould()
+        {
+            using var scope = _host.Services.CreateScope();
+            var serviceProvider = scope.ServiceProvider;
+
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                var service = serviceProvider.GetRequiredService<FakeCollectionConfigService>();
+
+                var configs = new FakeCollectionConfigEntity[]
+                {
+                    new FakeCollectionConfigEntity()
+                    {
+                        Id = 0,
+                        Number = 1,
+                        Value = 1,
+                    },
+                    new FakeCollectionConfigEntity()
+                    {
+                        Id = 0,
+                        Number = 2,
+                        Value = 2,
+                    },
+                    new FakeCollectionConfigEntity()
+                    {
+                        Id = 0,
+                        Number = 3,
+                        Value = 3,
+                    },
+                };
+
+                await service.SetAsync(configs);
+            });
+
+            Assert.Null(exception);
+
+            exception = await Record.ExceptionAsync(async () =>
+            {
+                var service = serviceProvider.GetRequiredService<FakeCollectionConfigService>();
+
+                var configs = new FakeCollectionConfigEntity[]
+                {
+                    new FakeCollectionConfigEntity()
+                    {
+                        Id = 0,
+                        Number = 1,
+                        Value = 1,
+                    },
+                    new FakeCollectionConfigEntity()
+                    {
+                        Id = 0,
+                        Number = 2,
+                        Value = 2,
+                    }
+                };
+
+                await service.SetAsync(configs);
             });
 
             Assert.Null(exception);
